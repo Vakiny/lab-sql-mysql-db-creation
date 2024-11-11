30ffdd374950e3a2810ee771898b6768cccb451a
Vasco <vasco-freire@hotmail.com>
2024-11-11 14:16:07 +0000

    Solved

---
+++ seeding.sql
@@ -0,0 +1,19 @@
+
+INSERT INTO Cars (id, VIN, manufacturer, model, year, color) VALUES
+(1, '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
+(2, 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
+(3, 'RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
+(4, 'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
+(5, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
+
+
+INSERT INTO Salespersons (id, staff_id, name, store) VALUES
+(1, 1, 'Petey Cruiser', 'Madrid'),
+(2, 2, 'Anna Sthesia', 'Barcelona'),
+(3, 3, 'Paul Molive', 'Berlin'),
+(4, 4, 'Gail Forcewind', 'Paris'),
+(5, 5, 'Paige Turner', 'Miami'),
+(6, 6, 'Bob Frapples', 'Mexico City'),
+(7, 7, 'Walter Melon', 'Amsterdam'),
+(8, 8, 'Shonda Leer', 'São Paulo');
+
